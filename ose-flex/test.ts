86fc4875554b7035674d8f521b1639636fa9b3b6
ssande-fwd <ssande@forwardedu.com>
2026-04-09 09:44:19 -0400

    test

--- ose-flex/test.ts
+++ ose-flex/test.ts
@@ -2,3 +2,13 @@
 // Type: functional
 //  flex(): number
 //  isPastThreshold(threshold: number, direction: opensciedEnums.OverUnder): boolean
+basic.forever(function () {
+    basic.showNumber(opensciedModules.flex1.flex())
+    if (opensciedModules.flex1.isPastThreshold(200, opensciedEnums.OverUnder.Over)) {
+        basic.showIcon(IconNames.Yes)
+    }
+    if (opensciedModules.flex1.isPastThreshold(200, opensciedEnums.OverUnder.Under)) {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+})
