30190ec0b1cfefc4bbc921eab53e16f94642eed5
ssande-fwd <ssande@forwardedu.com>
2026-04-09 09:43:02 -0400

    escape percent symbol

--- ose-flex/test.ts
+++ ose-flex/test.ts
@@ -1,14 +1,4 @@
-// Light Tests
+// Flex Tests
 // Type: functional
-//  illuminance(): number
+//  flex(): number
 //  isPastThreshold(threshold: number, direction: opensciedEnums.OverUnder): boolean
-basic.forever(function () {
-    basic.showNumber(opensciedModules.light1.illuminance())
-    if (opensciedModules.light1.isPastThreshold(200, opensciedEnums.OverUnder.Over)) {
-        basic.showIcon(IconNames.Yes)
-    }
-    if (opensciedModules.light1.isPastThreshold(200, opensciedEnums.OverUnder.Under)) {
-        basic.showIcon(IconNames.No)
-    }
-    basic.pause(2000)
-})
