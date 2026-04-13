654c698769cdbffc43456e3e64544420f20b8200
ssande-fwd <ssande@forwardedu.com>
2026-04-13 13:31:12 -0400

    test: ose-light sensible default

--- ose-light/test.ts
+++ ose-light/test.ts
@@ -4,11 +4,12 @@
 //  isPastThreshold(threshold: number, direction: opensciedEnums.OverUnder): boolean
 basic.forever(function () {
     basic.showNumber(opensciedModules.light1.illuminance())
-    if (opensciedModules.light1.isPastThreshold(200, opensciedEnums.OverUnder.Over)) {
+    basic.pause(1000)
+    if (opensciedModules.light1.isPastThreshold(10000, opensciedEnums.OverUnder.Over)) {
         basic.showIcon(IconNames.Yes)
     }
-    if (opensciedModules.light1.isPastThreshold(200, opensciedEnums.OverUnder.Under)) {
+    if (opensciedModules.light1.isPastThreshold(10000, opensciedEnums.OverUnder.Under)) {
         basic.showIcon(IconNames.No)
     }
-    basic.pause(2000)
+    basic.pause(1000)
 })
