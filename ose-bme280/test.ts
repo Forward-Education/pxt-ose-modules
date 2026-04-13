9311a9ef07531f762d87fb356ba04048c0d615e8
ssande-fwd <ssande@forwardedu.com>
2026-04-13 12:44:30 -0400

    test: functional ose test

--- ose-bme280/test.ts
+++ ose-bme280/test.ts
@@ -1,17 +1,56 @@
 // BME280 Tests
-// Type: compilation
+// Type: functional
 //  pressure(): number
 //  humidity(): number
 //  temperature(): number
 //  isPastThreshold(threshold: number, direction: opensciedEnums.OverUnder): boolean
 basic.forever(function () {
-    if (opensciedModules.airPressure.isPastThreshold(0, opensciedEnums.OverUnder.Over)) {
-        basic.showNumber(opensciedModules.airPressure.pressure())
+    basic.showString('P:' + Math.round(opensciedModules.airPressure.pressure()))
+    if(opensciedModules.airPressure.isPastThreshold(1000, opensciedEnums.OverUnder.Over)) {
+        basic.showIcon(IconNames.Yes)
     }
-    if (opensciedModules.humidity.isPastThreshold(0, opensciedEnums.OverUnder.Over)) {
-        basic.showNumber(opensciedModules.humidity.humidity())
+    if (opensciedModules.airPressure.isPastThreshold(1000, opensciedEnums.OverUnder.Under)) {
+        basic.showIcon(IconNames.No)
     }
-    if (opensciedModules.temperature.isPastThreshold(0, opensciedEnums.OverUnder.Over)) {
-        basic.showNumber(opensciedModules.temperature.temperature())
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showString('H:' + Math.round(opensciedModules.humidity.humidity()))
+    if (opensciedModules.humidity.isPastThreshold(45, opensciedEnums.OverUnder.Over)) {
+        basic.showIcon(IconNames.Yes)
     }
+    if (opensciedModules.humidity.isPastThreshold(45, opensciedEnums.OverUnder.Under)) {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showString('T:' + Math.round(opensciedModules.temperature.temperature()))
+    if (opensciedModules.temperature.isPastThreshold(22, opensciedEnums.OverUnder.Over)) {
+        basic.showIcon(IconNames.Yes)
+    }
+    if (opensciedModules.temperature.isPastThreshold(22, opensciedEnums.OverUnder.Under)) {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(500)
 })
+
+// BME280 Tests
+// Type: compilation
+//  pressure(): number
+//  humidity(): number
+//  temperature(): number
+//  isPastThreshold(threshold: number, direction: opensciedEnums.OverUnder): boolean
+// basic.forever(function () {
+//     if (opensciedModules.airPressure.isPastThreshold(0, opensciedEnums.OverUnder.Over)) {
+//         basic.showNumber(opensciedModules.airPressure.pressure())
+//     }
+//     if (opensciedModules.humidity.isPastThreshold(0, opensciedEnums.OverUnder.Over)) {
+//         basic.showNumber(opensciedModules.humidity.humidity())
+//     }
+//     if (opensciedModules.temperature.isPastThreshold(0, opensciedEnums.OverUnder.Over)) {
+//         basic.showNumber(opensciedModules.temperature.temperature())
+//     }
+// })
